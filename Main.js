8dc5bc7be994ea9b777ca1299dc273ad9c6cb1d8
BSK-BE4T5 <panfilodemon1@gmail.com>
2024-09-19 20:52:56 -0600

    Main PW Gen complete

--- Main.js
+++ Main.js
@@ -1,3 +1,25 @@
-function updateValue(val) {
-    document.getElementById("textbox").value = val;
+let cantidad = document.getElementById('cantidad');
+let boton = document.getElementById('Generar');
+
+const cadenaCaracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmopqrstuvwxyz0123456789'
+let password = '';
+
+function Generar(){
+
+    let NumeroDigitado = parseInt (cantidad.value);
+    if (cantidad < 6){
+        alert("la cantidad de caracteres tiene que ser mayor o igual a 6.");
+    }
+    
+
+    for(let i = 0; i < NumeroDigitado; i++){
+        let caracterAleatorio = cadenaCaracteres[Math.floor(Math.random() * cadenaCaracteres.length)];
+        console.log(caracterAleatorio);
+        password+=caracterAleatorio;
+
+    }
+    
+    console.log('password generado: ' + password);
+
 }
+
